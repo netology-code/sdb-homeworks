9aabcfb1cb5d7ac58e7bb2d5daba29ad14dbb2bb
Andrey Sotnikov <115862529+a-sotnikov@users.noreply.github.com>
2023-04-15 18:03:02 +0300

    Update consumer.py
    В актуальной версии pika у basic_consume другие аргументы, скрипт падает с ошибкой.

--- 11-04/consumer.py
+++ 11-04/consumer.py
@@ -11,5 +11,5 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
 channel.start_consuming()
